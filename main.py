6264a7126f553807c43ecdd710f24c5b795084d8
Jonatan Arnberg <jonte.arnberg@gmail.com>
2023-10-15 23:24:44 +0200

    rensade lite

--- main.py
+++ main.py
@@ -12,35 +12,36 @@ class SinglePerceptronTrainer:
         self.rate = 0.1
         self.iterations = []
 
-    def weighted_sum(self):
-        dotproduct = [0 for _ in self.feature]
-        for i in range(len(self.feature)):
-            for j in range(len(self.feature[i])):
-                dotproduct[i] += self.feature[i][j] * self.weight[j]  # Calculate weighted sum
-            dotproduct[i] += self.bias  
-            
-
-        self.dotproducts = dotproduct  # Update class variable
+    def weighted_sum(self, features:list):
+        features_copy = features.copy()
+        dotproduct = [0 for _ in features_copy]
+        for i in range(len(features_copy)):
+            for j in range(len(features_copy[i])):
+                dotproduct[i] += features_copy[i][j] * self.weight[j]
+            dotproduct[i] += self.bias
+        if modify_internal_state:
+            self.dotproducts = dotproduct
         return dotproduct
 
+
     #steg-funktion. 
-    # def activation_function(self, dotproduct:list):
-    #     #detta är en steg-funktion, ej sigmoid, denna är binär. den kan endast säga 1 eller 0. 
-    #     outputs = []
-    #     for x in dotproduct:
-    #         if x > 0:
-    #             outputs.append(1)
-    #         else:
-    #             outputs.append(0)
-    #     return outputs
+    def activation_function(self, dotproduct:list):
+        #detta är en steg-funktion, ej sigmoid, denna är binär. den kan endast säga 1 eller 0. 
+        outputs = []
+        for x in dotproduct:
+            if x > 0:
+                outputs.append(1)
+            else:
+                outputs.append(0)
+        return outputs
     
     #Sigmoid funktion. 
-    def activation_function(self,z):
-        outputs = []
-        for x in z: 
-            output = 1/(1 + np.exp(-x))
-            outputs.append(output)
-        return outputs
+    # def activation_function(self,z):
+    #     outputs = []
+    #     for x in z: 
+    #         output = 1/(1 + np.exp(-x))
+    #         outputs.append(output)
+    #     return outputs
 
     def loss_calculation(self, outputs:list):
         losses = []
@@ -57,36 +58,37 @@ class SinglePerceptronTrainer:
         
         '''
 
-        self.iterations = list(range(1,2000))
+        self.iterations = list(range(1,20))
         self.all_losses = []
         self.all_outputs = []
 
-        for x in range(1, 2000):
-            self.weighted_sum()
+        for x in range(1, 20):
+            self.weighted_sum(self.feature)
             outputs = self.activation_function(self.dotproducts)
             losses = self.loss_calculation(outputs)
             
-            self.all_outputs.append(outputs)  # Store outputs for visualization
-            self.all_losses.append(sum(losses) / len(losses))  # Store average loss for visualization
+            self.all_outputs.append(outputs)  # Store outputs for visualization (plot will show 3 lines)
+            self.all_losses.append(sum(losses) / len(losses))  # Store average loss for visualization (loss shows 1 line representing all 3 estimates)
             for i in range(len(outputs)):
                 error = self.target[i] - outputs[i]
                 for j in range(len(self.weight)):
                     self.weight[j] += self.rate * error * self.feature[i][j]
                 self.bias += self.rate * error
 
-        return self.iterations, self.all_losses, self.all_outputs #returnar bara för visualisation
+        return self.iterations, self.all_losses, self.all_outputs 
 
-    def test(self, new_feature):
-        z = (new_feature*self.weight) + self.bias
-        prediction = self.activation_function(z)
-        return prediction
+    # def test(self, new_feature):
+    #     dotproducts = self.weighted_sum(new_feature, modify_internal_state=False)
+    #     prediction = self.activation_function(dotproducts)
+    #     return prediction
 
 ai = SinglePerceptronTrainer()
 
 iterations, losses, outputs = ai.train()
 
+#printa estimates. 
 for x in ai.all_outputs:
-    print(x)
+    print(f"train:",x)
 
 #dessa figurer visar att förlusten minskar ju fler iterationer modellen tränas. 
 plt.figure(figsize=(10,5))
